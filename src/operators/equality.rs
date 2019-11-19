30807c950a34c4118f73010967381ff7356ddd53
Marvin Davieds <marvin.davieds@gmail.com>
2019-11-19 17:25:58 +0100

    use match for is_equal, rename to is_loose_equal, at missing conversions

--- src/operators/equality.rs
+++ src/operators/equality.rs
@@ -1,4 +1,4 @@
-use serde_json::Value;
+use serde_json::{Number, Value};
 
 pub fn compute_equality(arguments: &Value) -> Result<Value, String> {
     // Return true if the argument is null. Return false if the arguments are not are not null or
@@ -17,107 +17,82 @@ pub fn compute_equality(arguments: &Value) -> Result<Value, String> {
     let first: &Value = arr.get(0).unwrap_or_else(|| &Value::Null);
     let second: &Value = arr.get(1).unwrap_or_else(|| &Value::Null);
 
-    return Ok(Value::Bool(is_equal(first, second)));
+    return Ok(Value::Bool(is_loose_equal(first, second)));
 }
 
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Equality_comparisons_and_sameness
-pub fn is_equal(a: &Value, b: &Value) -> bool {
-    if a.is_null() {
-        b.is_null()
-    } else if a.is_number() {
-        if b.is_null() {
-            false
-        } else if b.is_number() {
-            a.as_f64().unwrap() == b.as_f64().unwrap()
-        } else if b.is_string() {
-            equal_number_string(a, b)
-        } else if b.is_boolean() {
-            equal_number_boolean(a, b)
-        } else if b.is_array() {
-            equal_number_array(a, b)
-        } else if b.is_object() {
-            false
-        } else {
-            unreachable!()
-        }
-    } else if a.is_string() {
-        if b.is_null() {
-            false
-        } else if b.is_number() {
-            equal_number_string(b, a)
-        } else if b.is_string() {
-            a == b
-        } else if b.is_boolean() {
-            equal_string_boolean(a, b)
-        } else if b.is_array() {
-            a.as_str().unwrap() == array_to_str(b)
-        } else if b.is_object() {
-            false
-        } else {
-            unreachable!()
-        }
-    } else if a.is_boolean() {
-        if b.is_null() {
-            false
-        } else if b.is_number() {
-            bool_to_number(a.as_bool().unwrap()) == b.as_f64().unwrap()
-        } else if b.is_string() {
-            equal_string_boolean(b, a)
-        } else if b.is_boolean() {
-            a == b
-        } else if b.is_array() {
-            false
-        } else if b.is_object() {
-            false
-        } else {
-            unreachable!()
-        }
-    } else if a.is_array() {
-        if b.is_null() {
-            false
-        } else if b.is_number() {
-            equal_number_array(b, a)
-        } else if b.is_string() {
-            &array_to_str(a) == b.as_str().unwrap()
-        } else if b.is_boolean() {
-            false
-        } else if b.is_array() {
-            a == b
-        } else if b.is_object() {
-            false
-        } else {
-            unreachable!()
-        }
-    } else if a.is_object() {
-        false
-    } else {
-        unreachable!()
+pub fn is_loose_equal(a: &Value, b: &Value) -> bool {
+    match a {
+        Value::Null => b.is_null(),
+        Value::Number(a) => match b {
+            Value::Null => false,
+            Value::Number(b) => a == b,
+            Value::String(b) => equal_number_string(a, b),
+            Value::Bool(b) => equal_number_boolean(a, *b),
+            Value::Array(_) => equal_number_array(a, b),
+            Value::Object(_) => false,
+        },
+        Value::String(a) => match b {
+            Value::Null => false,
+            Value::Number(b) => equal_number_string(b, a),
+            Value::String(b) => a == b,
+            Value::Bool(b) => equal_string_boolean(a, *b),
+            Value::Array(_) => a == &array_to_str(b),
+            Value::Object(_) => false,
+        },
+        Value::Bool(a) => match b {
+            Value::Null => false,
+            Value::Number(b) => equal_number_boolean(b, *a),
+            Value::String(b) => equal_string_boolean(b, *a),
+            Value::Bool(b) => a == b,
+            Value::Array(_) => equal_array_bool(b, *a),
+            Value::Object(_) => false,
+        },
+        Value::Array(_) => match b {
+            Value::Null => false,
+            Value::Number(b) => equal_number_array(b, a),
+            Value::String(b) => b == &array_to_str(a),
+            Value::Bool(b) => equal_array_bool(a, *b),
+            Value::Array(_) => a == b,
+            Value::Object(_) => false,
+        },
+        Value::Object(_) => false,
     }
 }
 
-fn equal_number_string(number_val: &Value, str_val: &Value) -> bool {
+fn equal_array_bool(array_val: &Value, bool_val: bool) -> bool {
+    let arr_str = array_to_str(array_val);
+    match str_to_number(&arr_str) {
+        // This matches for arrays [1] or [0], ... or [100] of course.
+        Some(arr_num) => arr_num == bool_to_number(bool_val),
+        // If it is not a number, interpret as a string. Might be [true] or [false].
+        None => arr_str == "true",
+    }
+}
+
+fn equal_number_string(number_val: &Number, str_val: &String) -> bool {
     let num1 = number_val.as_f64().unwrap();
-    match str_to_number(str_val.as_str().unwrap()) {
+    match str_to_number(str_val) {
         Some(num2) => num1 == num2,
         None => false,
     }
 }
 
-fn equal_number_boolean(number_val: &Value, bool_val: &Value) -> bool {
+fn equal_number_boolean(number_val: &Number, bool_val: bool) -> bool {
     let num1 = number_val.as_f64().unwrap();
-    let num2 = bool_to_number(bool_val.as_bool().unwrap());
+    let num2 = bool_to_number(bool_val);
     num1 == num2
 }
 
-fn equal_string_boolean(string_val: &Value, bool_val: &Value) -> bool {
-    let num2 = bool_to_number(bool_val.as_bool().unwrap());
-    match str_to_number(string_val.as_str().unwrap()) {
+fn equal_string_boolean(string_val: &String, bool_val: bool) -> bool {
+    let num2 = bool_to_number(bool_val);
+    match str_to_number(string_val) {
         Some(num1) => num1 == num2,
         None => false,
     }
 }
 
-fn equal_number_array(number_val: &Value, array_val: &Value) -> bool {
+fn equal_number_array(number_val: &Number, array_val: &Value) -> bool {
     let num1 = number_val.as_f64().unwrap();
     match str_to_number(&array_to_str(array_val)) {
         Some(num2) => num1 == num2,
@@ -145,7 +120,6 @@ fn bool_to_number(b: bool) -> f64 {
 }
 
 fn array_to_str(arr: &Value) -> String {
-    //let arr = arr.as_array().unwrap();
     let s: String = format!("{}", arr);
     // Remove `[` and `]`.
     s[1..s.len() - 1].to_owned()
@@ -161,25 +135,29 @@ mod tests {
         assert_eq!(array_to_str(&json!([1, 2])), "1,2");
     }
 
+    macro_rules! test_loose_equal {
+        ($a:expr, $b:expr, $expect:expr) => {
+            assert_eq!(is_loose_equal($a, $b), $expect);
+            assert_eq!(is_loose_equal($b, $a), $expect);
+        };
+    }
+
     #[test]
-    fn equal() {
-        // TODO: test for each is_equal(a,b) <=> is_equal(b,a)
-        let null = Value::Null;
-        assert_eq!(is_equal(&null, &null), true);
-        assert_eq!(is_equal(&json!(true), &json!(true)), true);
-        assert_eq!(is_equal(&json!(false), &json!(false)), true);
-        assert_eq!(is_equal(&json!("foo"), &json!("foo")), true);
-        assert_eq!(is_equal(&json!(0), &json!(0)), true);
-        assert_eq!(is_equal(&json!(0), &json!(false)), true);
-        assert_eq!(is_equal(&json!(""), &json!(false)), true);
-        assert_eq!(is_equal(&json!(""), &json!(0)), true);
-        assert_eq!(is_equal(&json!("0"), &json!(0)), true);
-        assert_eq!(is_equal(&json!("17"), &json!(17)), true);
-        assert_eq!(is_equal(&json!([1, 2]), &json!("1,2")), true);
-        assert_eq!(is_equal(&json!([1]), &json!(1)), true);
-        assert_eq!(is_equal(&json!(0), &Value::Null), false);
-        // TODO:
-        //assert_eq!(is_equal(&json!([1]), &json!(true)), true);
-        //assert_eq!(is_equal(&json!([true]), &json!(true)), true);
+    fn loose_equal() {
+        test_loose_equal!(&Value::Null, &Value::Null, true);
+        test_loose_equal!(&json!(true), &json!(true), true);
+        test_loose_equal!(&json!(false), &json!(false), true);
+        test_loose_equal!(&json!("foo"), &json!("foo"), true);
+        test_loose_equal!(&json!(0), &json!(0), true);
+        test_loose_equal!(&json!(0), &json!(false), true);
+        test_loose_equal!(&json!(""), &json!(false), true);
+        test_loose_equal!(&json!(""), &json!(0), true);
+        test_loose_equal!(&json!("0"), &json!(0), true);
+        test_loose_equal!(&json!("17"), &json!(17), true);
+        test_loose_equal!(&json!([1, 2]), &json!("1,2"), true);
+        test_loose_equal!(&json!([1]), &json!(1), true);
+        test_loose_equal!(&json!(0), &Value::Null, false);
+        test_loose_equal!(&json!([1]), &json!(true), true);
+        test_loose_equal!(&json!([true]), &json!(true), true);
     }
 }
